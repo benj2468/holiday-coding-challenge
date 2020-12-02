cfef10b9a9f451ed57fd57749d5c7374dea97772
benjamincape <benjcape@gmail.com>
2020-12-01 20:50:47 -0500

    day1

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -12,26 +12,21 @@ def twos(sig, k):
     while 0 <= p < q < len(sig):
         s = sig[p] + sig[q]
         if s == k:
-            break
+            return (p, q)
         if s > k:
             q -= 1
             p -= 1
         if s < k or p < 0:
             p += 1
-    if 0 <= p < q < len(sig):
-        assert(sig[p] + sig[q] == k)
-        return (p, q)
 
 
 def threes(sig, k):
     for r in range(len(sig)):
         two = twos(sig, k-sig[r])
         if two and not r in two:
-            break
-    if r < len(sig):
-        (p, q) = two
-        assert(sig[p] + sig[q] + sig[r] == k)
-        return (r, p, q)
+            (p, q) = two
+            assert(sig[p] + sig[q] + sig[r] == k)
+            return (r, p, q)
 
 def display(sig, l):
     vals = list(map(lambda i: sig[i], l))
